aaf8b8aa3e00543fd0f2f2d16a5c1711a919d6f7
Diplomat-Properties <diplomatnfc@gmail.com>
2025-07-09 13:28:45 +0400

    Update save.js

--- JS/save.js
+++ JS/save.js
@@ -38,11 +38,11 @@ function downloadVCard() {
   const vCardData = `
 BEGIN:VCARD
 VERSION:3.0
-FN:ADNAN ALBAKKOUR
+FN:Yamen Hammad 
 TITLE:Sales Manager
 ORG:RISE Properties
-TEL;TYPE=WORK,VOICE:+971586410997
-EMAIL:adnano@dpdxb.com
+TEL;TYPE=WORK,VOICE:+971565591186
+EMAIL:yamenhammad992@gmail.com
 URL:http://www.dpdxb.com
 ADR;TYPE=WORK:;;PO Box No 181702, Office NO.807, Opal Tower, Business Bay;Dubai;;UAE
 END:VCARD
@@ -53,7 +53,7 @@ END:VCARD
 
   const a = document.createElement('a');
   a.href = url;
-  a.download = 'ADNAN ALBAKKOUR.vcf';
+  a.download = 'Yamen Hammad.vcf';
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
